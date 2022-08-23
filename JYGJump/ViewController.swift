21657725f1f26042fa994bd3db04e7809581b554
cictec <laoyouhub@icloud.com>
2022-08-23 10:36:23 +0800

    Initial Commit

---
+++ JYGJump/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JYGJump
+//
+//  Created by MacMini on 2022/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
